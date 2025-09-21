d7f0db0824a43245492bdaf5884e16fe0ed30b1d
deris7 <pakidingd86@gmail.com>
2025-09-21 23:49:55 +0800

    profile.jsx not found

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Profile from "./pages/Profile";
+import Profile from "./pages/profile";
 
 export default function App() {
   return (
