d36fb419c1e3dd3dcb65ea489399d31875b57b2b
deris7 <pakidingd86@gmail.com>
2025-09-21 23:57:32 +0800

    profile.jsx not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Profile from "./pages/profile";
+import Profile from "./pages/Profile";
 
 export default function App() {
   return (
-    <>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </>
+    </HashRouter>
   );
 }
